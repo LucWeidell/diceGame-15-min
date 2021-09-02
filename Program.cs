a8ee1ae53e9829e7180c8de560407c0d0979f4f4
Luc Weidell <>
2021-09-02 09:45:11 -0600

    dice game complete just a small file in program

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ namespace diceGame
     {
         static void Main(string[] args)
         {
-            int numDice, numSides, numMod;
+            int numDice = 0;
+            int numSides = 0;
+            int numMod = 0;
             bool gettingNumber = true;
             while(gettingNumber)
             {
                 System.Console.Write("Number of Dice: ");
                 string line = System.Console.ReadLine();
-                if(!Int32.TryParse(line, out numDice)
+                if(!Int32.TryParse(line, out int numDice2))
                 {
+                    numDice = numDice2;
                     System.Console.WriteLine("Not a valid number of dice");
                 } else
                 {
@@ -25,7 +28,8 @@ namespace diceGame
             {
                 System.Console.Write("Number of Sides on Dice: ");
                 string line = System.Console.ReadLine();
-                if(!Int32.TryParse(line, out numSides){
+                if(!Int32.TryParse(line, out int numSides2)){
+                    numSides = numSides2;
                     System.Console.WriteLine("Not a valid number of dice");
                 } else
                 {
@@ -37,32 +41,33 @@ namespace diceGame
             {
                 System.Console.Write("Modifier for End Total: ");
                 string line = System.Console.ReadLine();
-                if(!Int32.TryParse(line, out numMod){
+                if(!Int32.TryParse(line, out int numMod2)){
+                    numMod = numMod2;
                     System.Console.WriteLine("Not a valid number of dice");
                 } else
                 {
                     gettingNumber = false;
                 }
             }
-            Random rnd = new Random()
-            List<int>results = new List<int>();
-            int result = 0 + numMod;
+            Random rnd = new Random();
+            // List<int> results = new List<int>();
+            int sum = 0 + numMod;
             System.Console.Write("Results: (");
             for (int i = 0; i <= numDice; i++)
             {
                 int randResult = rnd.Next(1, numSides);
-                result += randResult;
-                results.Add(randResult);
+                sum += randResult;
+                // results.Add(randResult);
                 if(i != numDice)
                 {
-                    System.Console.Write(randResult + ', ');
+                    System.Console.Write(randResult + ", ");
                 } else
                 {
-                    System.Console.Write(randResult + ')\n');
+                    System.Console.Write(randResult + ")\n");
 
                 }
             }
-            System.Console.WriteLine("Total: " + result);
+            System.Console.WriteLine("Total: " + sum);
         }
     }
 }
